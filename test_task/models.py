0682a80a9c4a372b3e0e91bbf3f38af0afe97074
ElenaVolchkova <sombradzi@gmail.com>
2021-05-21 09:00:33 +0300

    Changed application model, templates, manufacturer.id filter

--- test_task/models.py
+++ test_task/models.py
@@ -11,15 +11,6 @@ class Manufacturer(models.Model):
         return self.manufacturer_name
 
 
-class Product(models.Model):
-    create_product_date = models.DateTimeField(auto_now_add=True)
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, null=True, blank=True)
-    product_name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.product_name
-
-
 class Contract(models.Model):
     create_contract_date = models.DateTimeField(auto_now_add=True)
     description_contract = models.CharField(max_length=300, null=True)
@@ -31,9 +22,18 @@ class Contract(models.Model):
 class CreditApplication(models.Model):
     create_credit_date = models.DateTimeField(auto_now_add=True)
     description_application = models.CharField(max_length=300, null=True)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True)
-    contract = models.ForeignKey(Contract, on_delete=models.CASCADE)
+    contract = models.ForeignKey(Contract, on_delete=models.CASCADE, related_name='contract_applications')
 
     def __str__(self):
         return self.description_application
 
+
+class Product(models.Model):
+    create_product_date = models.DateTimeField(auto_now_add=True)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, null=True, blank=True)
+    product_name = models.CharField(max_length=100)
+    credit_application = models.ForeignKey(CreditApplication, on_delete=models.CASCADE, null=True,
+                                           blank=True)
+
+    def __str__(self):
+        return self.product_name
--- test_task/views.py
+++ test_task/views.py
@@ -3,14 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 from test_task.models import CreditApplication
 
+
 def contracts_list(request):
     contracts = CreditApplication.objects.all()
     context = {"contracts": contracts}
     return render(request, "test_task/contracts_list.html", context)
 
-#
-def contract_detail(request, pk):
-    contract = CreditApplication.objects.get(pk=pk)
-    context = {"contract": contract}
-    return render(request, "test_task/contract_detail.html", context)
 
+def contract_detail(request, pk):
+    contract = CreditApplication.objects.filter(contract__pk=pk).values_list("product__manufacturer__id",
+                                                                             flat=True).distinct()
+    context = {"contract": contract}
+    # import pdb;
+    # pdb.set_trace()
+    return render(request, "test_task/contract_detail.html", context)
